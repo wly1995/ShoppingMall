7341c6a04952eb6c64ae1b9aac859aac251d2c75
wly1995 <550612304@qq.com>
2017-03-01 18:23:49 +0800

    活动页面H5页面添加数据到购物车

--- app/src/main/java/com/atguigu/shoppingmall/shoppingcart/activity/CallCenterActivity.java
+++ app/src/main/java/com/atguigu/shoppingmall/shoppingcart/activity/CallCenterActivity.java
@@ -1,9 +1,72 @@
 package com.atguigu.shoppingmall.shoppingcart.activity;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.atguigu.shoppingmall.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by 万里洋 on 2017/3/1.
  */
 
-public class CallCenterActivity {
+public class CallCenterActivity extends AppCompatActivity {
+    @InjectView(R.id.webview)
+    WebView webview;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_center);
+        ButterKnife.inject(this);
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        //设置检索缓存的
+        webSettings.setCacheMode(webSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        webview.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    view.loadUrl(request.getUrl().toString());
+                }
+                return true;
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return false;
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+        webview.loadUrl("http://www6.53kf.com/webCompany.php?arg=10007377&style=2&kflist=off&kf=info" +
+                "@atguigu.com,video@atguigu.com,public@atguigu.com,3069368606@qq.com,215648937@qq.com,sudan" +
+                "@atguigu.com,sszhang@atguigu.com&zdkf_type=1&language=zh-cn&charset=gbk&referer=http%3A%2F%2" +
+                "Fatguigu.com%2F&keyword=&tfrom=1&tpl=crystal_blue&uid=35fd8e6a8f0f3e7a1caedcc583f2f7a7&timeStamp=" +
+                "1488340364404&ucust_id=");
+    }
+    //按返回键时， 不退出程序而是返回上一浏览页面
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
