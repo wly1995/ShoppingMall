4239e6e3e49e2456ce30b9434c1a73e8422a554d
wly1995 <550612304@qq.com>
2017-03-03 13:50:54 +0800

    分类页面的框架

---
+++ app/src/main/java/com/atguigu/shoppingmall/type/fragment/ListFragment.java
@@ -0,0 +1,30 @@
+package com.atguigu.shoppingmall.type.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.shoppingmall.base.BaseFragment;
+
+/**
+ * Created by 万里洋 on 2017/3/3.
+ */
+
+public class ListFragment extends BaseFragment {
+    private TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(25);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("分类Fragment内容");
+    }
+}
